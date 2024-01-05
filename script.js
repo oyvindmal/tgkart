8ab5d90e7bb3c725a7e64e169a2bdf2573d76a35
Øyvind Malin <oyvindmal@outlook.com>
2024-01-05 09:54:11 +0100

    start canvas

--- script.js
+++ script.js
@@ -13,4 +13,18 @@ $(document).ready(function() {
         // Toggle class 'active' for the button itself
         $(this).toggleClass("active");
     });
+
+    var canvas = document.getElementById("mycanvas");
+    var context = canvas.getContext("2d");
+
+    // Create an image object
+    var img = new Image();
+
+    // Set the source of the image (replace 'path/to/your/image.png' with the actual path)
+    img.src = '/mapfiles/basemap.png';
+
+    // Draw the image onto the canvas when it's loaded
+    img.onload = function() {
+        context.drawImage(img, 0, 0);
+    };
 });
